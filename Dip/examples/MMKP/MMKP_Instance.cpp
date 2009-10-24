c4c0409a6a55ed910e9f70ab88513bbf07e76a00
Matthew Galati <matthew.galati@gmail.com>
2009-10-24 04:56:35 +0000

    mmkp

--- Dip/examples/MMKP/MMKP_Instance.cpp
+++ Dip/examples/MMKP/MMKP_Instance.cpp
@@ -16,6 +16,79 @@
 //===========================================================================//
 
 //===========================================================================//
+#if 0
+void MMKP_Instance::readInstanceSimon(string & fileName){
+   
+   int      k, i, j, ij, numIJ, dummy;
+   int      status = 0;
+   string   dummyStr;
+   ifstream is;
+   
+
+   /*
+     # MMKP instances generated from definition file: tmp2.xml
+     # serie_name    rp_hep_hec_strong
+     # num_instances 1
+     instance_id     1
+     num_class       10
+     num_dimension   5
+     # class_id      1
+     num_item        5
+     1       2       2       3       3       3
+     3       4       4       4       4       5
+     5       6       6       6       6       6
+     7       8       8       8       8       8
+     9       10      10      10      10      10
+     # class_id      2
+     num_item        5
+     ...
+     c:      518     524     531     540     553
+   */
+   status = UtilOpenFile(is, fileName.c_str());   
+   if(status)
+      throw UtilException("Failed to read instance",
+                          "readInstance", "MMKP_Instance");
+   is.getline();
+   is.getline();
+   is.getline();
+   is.getline();
+   is >> dummyStr >> m_nGroupRows //num_class
+      >> dummyStr >> m_nKnapRows;//num_dimension
+      >> m_nGroupCols
+            ////////STOP
+   
+   //---
+   //--- allocate memory for capacity, value and weight
+   //---
+   numIJ      = m_nGroupCols * m_nGroupRows;   
+   m_capacity = new double[m_nKnapRows];
+   m_value    = new double[numIJ];
+   m_weight   = new double*[m_nKnapRows];   
+   if(!(m_capacity && m_value && m_weight))
+      throw UtilExceptionMemory("readInstance", "MMKP_Instance");
+	     
+   for(k = 0; k < m_nKnapRows; k++){
+      m_weight[k] = new double[numIJ];
+      if(!m_weight[k])
+         throw UtilExceptionMemory("readInstance", "MMKP_Instance");
+      is >> m_capacity[k];
+   }
+   
+   for(i = 0; i < m_nGroupRows; i++){
+      is >> dummy;
+      for(j = 0; j < m_nGroupCols; j++){
+         ij = getIndexIJ(i,j);
+         is >> m_value[ij];
+         for(k = 0; k < m_nKnapRows; k++){
+            is >> m_weight[k][ij];
+         }
+      }
+   }
+   
+   is.close();
+}
+#endif
+//===========================================================================//
 void MMKP_Instance::readInstance(string & fileName){
    
    int      k, i, j, ij, numIJ, dummy;
