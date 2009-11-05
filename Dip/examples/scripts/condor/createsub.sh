a93144c63c53df12bed868ca6c498c51df2752fd
Matthew Galati <matthew.galati@gmail.com>
2009-11-05 05:51:46 +0000

    scripts

--- Dip/examples/scripts/condor/createsub.sh
+++ Dip/examples/scripts/condor/createsub.sh
@@ -1,15 +1,25 @@
 #c  = cpm
 #c2 = cpm+dc
 #p  = pc
-#p  = pc+solveMasterAsIp
+#p  = pc+solveMasterAsIp 
 #d  = direct
-for i in atm milpblock mmkp gap
+
+EXT=_inferno
+
+for i in atm milpblock gap
 do
-  ./createsub_lnx_${i}.sh ${i}p $1
-  ./createsub_lnx_${i}.sh ${i}p2 $1
-  ./createsub_lnx_${i}.sh ${i}c $1
-  ./createsub_lnx_${i}.sh ${i}c2 $1
-  ./createsub_lnx_${i}.sh ${i}d $1
+  ./createsub_lnx_${i}${EXT}.sh ${i}p $1
+  ./createsub_lnx_${i}${EXT}.sh ${i}c $1
+  ./createsub_lnx_${i}${EXT}.sh ${i}c2 $1
+  ./createsub_lnx_${i}${EXT}.sh ${i}d $1
+  ./createsub_lnx_${i}${EXT}.sh ${i}p2 $1
+done
+for i in mmkp
+do
+  ./createsub_lnx_${i}${EXT}.sh ${i}p $1
+  ./createsub_lnx_${i}${EXT}.sh ${i}c $1
+  ./createsub_lnx_${i}${EXT}.sh ${i}c2 $1
+  ./createsub_lnx_${i}${EXT}.sh ${i}d $1
 done
 # for i in #vrp
 # do
