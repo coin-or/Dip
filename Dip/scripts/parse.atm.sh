ce96f4c2d0d3f9e703c3e3dac352f33b24dd622c
Matthew Galati <matthew.galati@gmail.com>
2009-11-10 18:54:03 +0000

    wip

--- Dip/scripts/parse.atm.sh
+++ Dip/scripts/parse.atm.sh
@@ -1,15 +1,10 @@
-./parse.awk.sh ../data/ATM/results/atm_pc1.3600.txt 3600
-./parse.awk.sh ../data/ATM/results/atm_pc1.300.txt 300
-./parse2.awk.sh ../data/ATM/results/atm_cpx11.3600.txt 3600
-./parse2.awk.sh ../data/ATM/results/atm_cpx11.300.txt 300
-
-awk '{print $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10 "\t" $11 "\t" $12 "\\\\ \\hline"}' cpx.3600 > cpx.3600.2
-awk '{print $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10 "\t" $11 "\t" $12 "\\\\ \\hline"}' cpx.300 > cpx.300.2
-
-paste pc.300 cpx.300.2 > results.300
-paste pc.3600 cpx.3600.2 > results.3600
+./parse.awk.sh ../data/ATM/results/atm_pc_1.3600.txt 3600 1    #price+branch
+./parse.awk.sh ../data/ATM/results/atm_pc_2.3600.txt 3600 2    #pc
+./parse2.awk.sh ../data/ATM/results/atm_cpx11.3600.txt 3600    
 
 
-rm pc.3600 pc.300
-rm cpx.3600 cpx.300
-rm cpx.3600.2 cpx.300.2
+awk '{print $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10 "\t" $11 "\t" $12}' pc.3600.2 > tmp.2
+awk '{print $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10 "\t" $11 "\t" $12 "\\\\ \\hline"}' pc.3600.1 > tmp.1
+
+paste cpx.3600 pc.3600.2 pc.3600.1 > results.3600
+
--- Dip/scripts/parse.awk.sh
+++ Dip/scripts/parse.awk.sh
@@ -1,9 +1,16 @@
 #parser for decomp output
 TIME=$2
+ID=$3
 
 awk -F'.' '{print $1 " ->" $0'} $1 > tmp
 awk '
-{printf "Instance= %15s LB= %12.3f UB= %12.3f Gap= %10.4f Nodes= %10d Cpu= %10.2f Real= %10.2f\n",$1,$5,$7,($7-$5)/$7,$9,$15,$21;
+{
+if($7>1.0e15){
+printf "Instance= %15s LB= %12.3f UB= %12s Gap= %s Nodes= %10d Cpu= %10.2f Real= %10.2f\n",$1,$5,999999,999999,$9,$15,$21;
+}
+else{
+printf "Instance= %15s LB= %12.3f UB= %12.3f Gap= %10.4f Nodes= %10d Cpu= %10.2f Real= %10.2f\n",$1,$5,$7,($7-$5)/$7,$9,$15,$21;
+}
 }' tmp > tmp2
 
 awk -F'_' '{print $2 " " $3 " " $4}' tmp2 > tmp3
@@ -38,7 +45,7 @@ else{
  }
 }
 }' tmp3 > tmp4
-sort -n +0 -1 +2 -3 tmp4 > pc.${TIME}
+sort -n +0 -1 +2 -3 tmp4 > pc.${TIME}.${ID}
 
 rm tmp
 rm tmp2
