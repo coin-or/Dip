97fecc2813acf2847eba3221edc37edf07d20eee
Matthew Galati <matthew.galati@gmail.com>
2009-11-10 19:45:52 +0000

    results

--- Dip/data/MILP/block/retail.pc.sh
+++ Dip/data/MILP/block/retail.pc.sh
@@ -1,9 +1,8 @@
-EXE=/home/magh/AFS/coin-Decomp/build/Decomp/examples/MILPBlock/decomp_milpblock
-PARM=/home/magh/AFS/coin-Decomp/Decomp/data/MILP/block/retail.pc.parm
+EXE=/home/magh/AFS/COIN/coin-Dip/build-O/Dip/examples/MILPBlock/decomp_milpblock
+PARM=/home/magh/AFS/COIN/coin-Dip/Dip/data/MILP/block/retail.pc.parm
 
 for i in 3 4 6 20 22 27 31 33
-#for i in 33
 do
   echo "Running " ${i}
-${EXE} --param ${PARM} --MILPBlock:Instance retail${i} --MILPBlock:BlockFile retail${i}.block > retail${i}.pc.txt 2> retail${i}.pc.err
+${EXE} --param ${PARM} --MILPBlock:Instance retail${i} --MILPBlock:BlockFile retail${i}.block > log/retail${i}.pc.txt 2> log/retail${i}.pc.err
 done
