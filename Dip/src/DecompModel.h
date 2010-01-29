3e7a0019af445997016618e5d9cbaebe3b18427b
Ted Ralphs <ted@lehigh.edu>
2010-01-29 16:46:20 +0000

    Updating copright statements, etc.

--- Dip/src/DecompModel.h
+++ Dip/src/DecompModel.h
@@ -1,10 +1,16 @@
 //===========================================================================//
-// This file is part of the Decomp Solver Framework.                         //
+// This file is part of the DIP Solver Framework.                            //
 //                                                                           //
-// Decomp is distributed under the Common Public License as part of the      //
+// DIP is distributed under the Eclipse Public License as part of the        //
 // COIN-OR repository (http://www.coin-or.org).                              //
 //                                                                           //
 // Author: Matthew Galati, SAS Institute Inc. (matthew.galati@sas.com)       //
+//                                                                           //
+// Conceptual Design: Matthew Galati, SAS Institute Inc.                     //
+//                    Ted Ralphs, Lehigh University                          //
+//                                                                           //
+// Conceptual Design: Matthew Galati, SAS Institute Inc.                     //
+//                    Ted Ralphs, Lehigh University                          //
 //                                                                           //
 // Copyright (C) 2002-2009, Lehigh University, Matthew Galati, and Ted Ralphs//
 // All Rights Reserved.                                                      //
