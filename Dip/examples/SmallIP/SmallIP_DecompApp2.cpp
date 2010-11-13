1b1eb9864182197d765cebe886e7e65eda44adc6
Matthew Galati <matthew.galati@gmail.com>
2010-11-13 23:30:26 +0000

    osicbc log level

--- Dip/examples/SmallIP/SmallIP_DecompApp2.cpp
+++ Dip/examples/SmallIP/SmallIP_DecompApp2.cpp
@@ -158,6 +158,10 @@ void SmallIP_DecompApp::createModels(){
    //--- load the OSI object to be used in solve relaxed
    //---
    m_osi.messageHandler()->setLogLevel(0);      
+   //m_osi.setHintParam(OsiDoReducePrint, true, OsiHintDo);
+#ifdef __DECOMP_IP_CBC__   
+   m_osi.getModelPtr()->setLogLevel(0);
+#endif
    m_osi.loadProblem(*m_modelPart1.getMatrix(),
 		     m_modelPart1.getColLB(),
 		     m_modelPart1.getColUB(),
