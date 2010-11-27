117b6701206c46a8d35b14286217b01b9b219c09
Matthew Galati <matthew.galati@gmail.com>
2010-11-27 19:35:57 +0000

    some cleanup

--- Dip/src/DecompApp.h
+++ Dip/src/DecompApp.h
@@ -17,16 +17,16 @@
 #ifndef DECOMP_APP_INCLUDED
 #define DECOMP_APP_INCLUDED
 
-/*-----------------------------------------------------------------------*/
+//===========================================================================//
 #include "UtilParameters.h"
 #include "DecompParam.h"
 #include "DecompModel.h"
 #include "DecompSolution.h"
 #include "DecompConstraintSet.h"
-
+//===========================================================================//
 class DecompAlgo;
 
-/*-----------------------------------------------------------------------*/
+//===========================================================================//
 /*!
  * \class DecompApp
  * \brief
@@ -35,15 +35,9 @@ class DecompAlgo;
  * The main application class where the user will define the model
  * decomposition and define any application specific methods.
  *
- * \see
- * DecompConstraintSet
- * DecompParam
- *
- * \todo
- * Clone a monkey.
  */
 
-/*-----------------------------------------------------------------------*/
+//===========================================================================//
 class DecompApp{
 
 private:
@@ -51,8 +45,6 @@ private:
     * Store the name of the class (for logging/debugging) - "who am I?"
     */
    string m_classTag;
-
-
   
 protected:
    /**
@@ -60,119 +52,63 @@ protected:
     */
    ostream * m_osLog;
 
-   //should be in DecompApp?
-   double      m_bestKnownLB;
-   double      m_bestKnownUB;
+   /**
+    * The best known LB/UB for this application (if known, for debugging).
+    */
+   double m_bestKnownLB;
+   double m_bestKnownUB;
 
   
 public:
    /**
     *  Parameters.
     */
-   DecompParam m_param;//need?
+   DecompParam m_param;
   
    /**
-    *  Model data object.
+    *  Model data: objective function 
     */  
-   const double                    * m_objective;
-   DecompAppModel                    m_modelCore;
-   map<int, DecompAppModel>          m_modelRelax;
+   const double * m_objective;
+
+   /**
+    *  Model data: the core model (A'')
+    */  
+   DecompAppModel m_modelCore;
+
+   /**
+    *  Model data: the relaxed model(s) (A')
+    */  
+   map<int, DecompAppModel> m_modelRelax;
+
+   /**
+    *  Model data: the relaxed (nested) model(s) (A')
+    */  
    map<int, vector<DecompAppModel> > m_modelRelaxNest;   
 
    /**
-    * Pointers to the algorithm objects.
-    *   NOTE: only for the advanced user -- this allows the user 
-    *         to really mess things up.
+    * Pointer to the base algorithmic object.
+    *   NOTE: only for the advanced user
     */
-   DecompAlgo *           m_decompAlgo;
+   DecompAlgo * m_decompAlgo;
    
 public:
-   //set log file
    /**
-      Initialize the DecompApp data.
-   */
+    * @name Helper functions.
+    * @{
+    */
+
+   /**
+    * Print startup message to log.
+    */
    void startupLog();
 
+   //TODO:
    //base layer needs to do some kind of check to make sure this actually
    //got done - but also nice to have user version... so base createModel
    //and user uesrCreateModel() which is pure?, in base userCreateModel
    //gets called and checked that it returns good information
    int createModel();  
 
-
-   /**
-      Initialize the dual vector for PhaseII of PC. 
-      
-      This is only called when dual stabilization is used, i.e.,
-      when m_param.DualStab > 0.      
-   */
-   //virtual void initDualVector(vector<double> & dualVector){
-   //}
-
-   /**
-    * Get the dual vector for PhaseII of PC. 
-    *
-    * This is only called when dual stabilization is used, i.e.,
-    * when m_param.DualStab > 0.      
-   */
-   //virtual void getDualVectorMutable(vector<double> & dualVector){
-   //}
-
-   virtual bool APPisUserFeasible(const double * x,
-                                  const int      n_cols,
-                                  const double   tolZero){
-      return true;
-   };
-
-   virtual int APPheuristics(const double            * xhat,
-			     const double            * origCost,
-			     vector<DecompSolution*> & xhatIPFeas){
-      return 0;
-   }
-
-   /**
-    * This function allows the user to return their own dual vector
-    * to be used in the generation of new variables (in the reduced-cost
-    * calculation).
-    *
-    * For reference, the user is given the dual vector from the restricted
-    * master (or the stabilized dual, if using m_param.DualStab).
-    */
-   virtual const double * getDualForGenerateVars(const double * dual){
-      return 0;
-   }
-  
-   virtual int generateInitVars(DecompVarList & initVars);
-   
-   virtual int generateCuts(const double  * x, 
-			    DecompCutList & newCuts);
-
-   virtual void solveRelaxedWhich(vector<int> & blocksToSolve){};
-      
-   virtual DecompSolverStatus solveRelaxed(const int          whichBlock,
-                                           const double     * redCostX,
-                                           const double       convexDual,
-                                           DecompVarList    & varList){
-      return DecompSolStatNoSolution;
-   }
-   virtual DecompSolverStatus solveRelaxedNest(const int          whichBlock,
-                                               const double     * redCostX,
-                                               const double       convexDual,
-                                               DecompVarList    & varList){
-      return DecompSolStatNoSolution;
-   }
-
-
-   virtual void printOriginalColumn(const int   index, 
-                                    ostream   * os = &cout) const;
-  
-   //TODO: change api so colNames comes from modelCore if exists
-   //  rather than - to simplify API 
-   virtual void printOriginalSolution(const int              n_cols, 
-                                      const vector<string> & colNames,
-                                      const double         * solution,
-                                      ostream              * os = &cout) const;
-   
    inline const double getBestKnownLB() const {return m_bestKnownLB;}
    inline const double getBestKnownUB() const {return m_bestKnownUB;}
 
@@ -231,22 +167,117 @@ public:
       m_modelRelaxNest[blockId].push_back(appModel);
    }
    
+   /**
+    * Get a pointer to the base algorithm class
+    */
    inline DecompAlgo * getDecompAlgo() const  {
       return m_decompAlgo;
    }
 
+
+   /**
+    * @}
+    */
+
+
 public:
+   /**
+    * @name Interface methods for user derivation (virtual).
+    * @{
+    */
+
+   /**
+      Initialize the dual vector for PhaseII of PC. 
+      
+      This is only called when dual stabilization is used, i.e.,
+      when m_param.DualStab > 0.      
+   */
+   //virtual void initDualVector(vector<double> & dualVector){
+   //}
+
+
+   virtual bool APPisUserFeasible(const double * x,
+                                  const int      n_cols,
+                                  const double   tolZero){
+      return true;
+   };
+
+   virtual int APPheuristics(const double            * xhat,
+			     const double            * origCost,
+			     vector<DecompSolution*> & xhatIPFeas){
+      return 0;
+   }
+
+   /**
+    * This function allows the user to return their own dual vector
+    * to be used in the generation of new variables (in the reduced-cost
+    * calculation).
+    *
+    * For reference, the user is given the dual vector from the restricted
+    * master (or the stabilized dual, if using m_param.DualStab).
+    */
+   virtual const double * getDualForGenerateVars(const double * dual){
+      return 0;
+   }
+  
+   virtual int generateInitVars(DecompVarList & initVars);
+   
+   virtual int generateCuts(const double  * x, 
+			    DecompCutList & newCuts);
+
+   virtual void solveRelaxedWhich(vector<int> & blocksToSolve){};
+      
+   virtual DecompSolverStatus solveRelaxed(const int          whichBlock,
+                                           const double     * redCostX,
+                                           const double       convexDual,
+                                           DecompVarList    & varList){
+      return DecompSolStatNoSolution;
+   }
+   virtual DecompSolverStatus solveRelaxedNest(const int          whichBlock,
+                                               const double     * redCostX,
+                                               const double       convexDual,
+                                               DecompVarList    & varList){
+      return DecompSolStatNoSolution;
+   }
+
+
+   virtual void printOriginalColumn(const int   index, 
+                                    ostream   * os = &cout) const;
+  
+   //TODO: change api so colNames comes from modelCore if exists
+   //  rather than - to simplify API 
+   virtual void printOriginalSolution(const int              n_cols, 
+                                      const vector<string> & colNames,
+                                      const double         * solution,
+                                      ostream              * os = &cout) const;
+
+   /**
+    * @}
+    */
+
+   
+
+public:
+   /**
+    * Constructor for base DecompApp class. This accepts a generic parameters
+    * object (UtilParameters) and reads in the parameter settings into the 
+    * DecompApp paramter object. 
+    */
    DecompApp(UtilParameters & utilParam) :
-      m_classTag  ("D-APP"),
-      m_osLog(&cout), 
-      m_bestKnownLB(-1e75),
-      m_bestKnownUB(1e75),
-      m_objective(NULL),
-      m_decompAlgo(NULL)
+      m_classTag   ("D-APP"),
+      m_osLog      (&cout  ), 
+      m_bestKnownLB(-1e75  ),
+      m_bestKnownUB( 1e75  ),
+      m_objective  (0      ),
+      m_decompAlgo (0      )
    {    
       m_param.getSettings(utilParam);
       startupLog();    
    };
+
+   /**
+    * Destructor.
+    */
    virtual ~DecompApp() {};
 };
 
