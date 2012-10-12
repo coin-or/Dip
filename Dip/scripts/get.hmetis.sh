a7036e4dbc2eb2df5ed27f70912bfc8cad5d8438
Jiadong Wang <jiadwang@gmail.com>
2012-10-12 19:27:22 +0000

    add scripts to downlaod hmetis, patoh and make test instances data

---
+++ Dip/scripts/get.hmetis.sh
@@ -0,0 +1,13 @@
+cd ../../ThirdParty
+
+wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/hmetis/hmetis-1.5-linux.tar.gz
+
+gunzip hmetis-1.5-linux.tar.gz
+
+tar -xvf hmetis-1.5-linux.tar
+
+rm hmetis-1.5-linux.tar
+
+cd ./hmetis-1.5-linux
+
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/hmetis.h
---
+++ Dip/scripts/get.test_data.sh
@@ -0,0 +1,13 @@
+cd ../../Data/Sample
+
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/atm_5_10_1.block
+
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/atm_5_10_1.mps
+
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/retail27.mps
+
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/retail27.block
+
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/wedding_17.mps
+
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/wedding_17.block
