f0413f052c6f23221fc8eb0370c9f9d23a5a3a1d
Jiadong Wang <jiadwang@gmail.com>
2012-10-12 23:43:35 +0000

    update a instance

--- Dip/scripts/get.test_data.sh
+++ Dip/scripts/get.test_data.sh
@@ -1,13 +1,13 @@
-cd ../../Data/Sample
+cd ../../build_informs/Data/Sample
 
 wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/atm_5_10_1.block
 
 wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/atm_5_10_1.mps
 
-wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/retail27.mps
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/retail3.mps
 
-wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/retail27.block
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/retail3.block
 
-wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/wedding_17.mps
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/wedding_16.mps
 
-wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/wedding_17.block
+wget http://coral.ie.lehigh.edu/~jiadongwang/DIP/make_test/wedding_16.block
