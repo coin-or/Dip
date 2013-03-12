673ee926ba4b1eaaa26617d48a3beb87e4096b18
Ted Ralphs <ted@lehigh.edu>
2013-03-12 23:20:18 +0000

    Put all dependent libraries in Dippy module

--- Dip/src/Dippy/__init__.py
+++ Dip/src/Dippy/__init__.py
@@ -1,28 +1,9 @@
 import sys
 
-if sys.platform == 'linux2':
-    try:
-        import path
-    except ImportError:
-        pass
+try:
+    import path
+except ImportError:
+    pass
 
-    import ctypes
-
-    ctypes.CDLL('libm.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libblas.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('liblapack.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libz.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libbz2.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libCoinUtils.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libOsi.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libClp.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libOsiClp.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libCgl.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libCbc.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libCbcSolver.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libOsiCbc.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libAlps.so', mode=ctypes.RTLD_GLOBAL)
-    ctypes.CDLL('libDecomp.so', mode=ctypes.RTLD_GLOBAL)
-    
 from dippy import *
 
