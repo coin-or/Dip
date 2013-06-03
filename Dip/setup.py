f45315199908389740ac6af251b9a2e3fc23eaa5
Ted Ralphs <ted@lehigh.edu>
2013-06-03 22:09:00 +0000

    Fixes for Python setup

--- Dip/setup.py
+++ Dip/setup.py
@@ -6,70 +6,11 @@ from setuptools import setup, Extension
 import subprocess, os, sys
 from os.path import join, dirname
 
-
 PROJECT = 'dippy'
 VERSION = '1.4.3'
 URL = 'https://projects.coin-or.org/Dip/wiki/Dippy'
 AUTHOR_EMAIL = u''
 DESC = 'DIP Python Interface'
-
-def get_libs():
-       
-    p = subprocess.Popen(['pkg-config', '--libs', 'dip'],
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    stdout_output, stderr_output = p.communicate()
-
-    if p.returncode != 0 :
-        raise Exception('pkg-config terminated with status:'+
-                        '%d. stderr follows: %s' 
-                        % (p.returncode, stderr_output))
-    elif stderr_output:
-        print stderr_output
-
-    lib_dirs = [flag[2:] for flag in stdout_output.split()
-                if flag.startswith('-L')]
-
-    libs =     [flag[2:] for flag in stdout_output.split()
-                if flag.startswith('-l')]
-
-    return lib_dirs, libs
-
-def get_includes():
-
-    p = subprocess.Popen(['pkg-config', '--cflags', 'dip'],
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    stdout_output, stderr_output = p.communicate()
-
-    if p.returncode != 0 :
-        raise Exception('pkg-config terminated with status: %d. stderr' +
-                        'follows: %s' % (p.returncode, stderr_output))
-    elif stderr_output:
-        print stderr_output
-
-    inc_dirs = [flag[2:] for flag in stdout_output.split()
-                if flag.startswith('-I')]
-
-    return inc_dirs
-
-def get_defines():
-
-    p = subprocess.Popen(['pkg-config', '--cflags', 'dip'],
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    stdout_output, stderr_output = p.communicate()
-
-    if p.returncode != 0 :
-        raise Exception('pkg-config terminated with status: %d. stderr' +
-                        'follows: %s' % (p.returncode, stderr_output))
-    elif stderr_output:
-        print stderr_output
-
-    defines = [flag[2:] for flag in stdout_output.split()
-               if flag.startswith('-D')]
-
-    return defines
 
 def read_file(file_name):
 
@@ -80,19 +21,24 @@ def read_file(file_name):
 
     return open(file_path).read()
 
-pkg_config_path = os.environ['PKG_CONFIG_PATH']
+def get_libs(coin_install_dir):
+       
+    link_line = read_file(join(coin_install_dir, 'doc', 'Dip', 
+                               'dip_addlibs.txt'))
 
-if pkg_config_path is None:
-    raise Exception('Please set the environment variable PKG_CONFIG_PATH' +
-                    'to the location of the COIN pkgconfig directory')
+    libs = [flag[:-4] for flag in link_line.split() if flag.endswith('.lib')]
 
-library_dirs, libraries = get_libs()
+    return libs
 
-include_dirs = get_includes()
+coin_install_dir = os.environ['COIN_INSTALL_DIR']
 
-macros = get_defines()
+if coin_install_dir is None:
+    raise Exception('Please set the environment variable COIN_INSTALL_DIR' +
+                    'to the location of the COIN installation')
 
-macros.append(('__DECOMP_LP_CLP__', None))
+libraries = get_libs(coin_install_dir)
+
+macros = [('__DECOMP_LP_CLP__', None)]
 
 files = ['DippyDecompAlgo.cpp',
          'DippyDecompApp.cpp',
@@ -106,8 +52,6 @@ sources = [join('src/dippy', f) for f in files]
 modules=[Extension('_dippy', 
                    sources, 
                    libraries=libraries,
-                   include_dirs=include_dirs,
-                   library_dirs=library_dirs,
                    define_macros=macros)]
 
 setup(name=PROJECT,
