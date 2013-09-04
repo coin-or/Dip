a4fc2abc3d5aaaf26b12c315c04d29ee25131dd5
Ted Ralphs <ted@lehigh.edu>
2013-09-04 23:13:23 +0000

    Syncing with trunk at r826

--- Dip/setup.py
+++ Dip/setup.py
@@ -7,7 +7,7 @@ import subprocess, os, sys
 from os.path import join, dirname
 
 PROJECT = 'dippy'
-VERSION = '1.4.3'
+VERSION = '1.9.0'
 URL = 'https://projects.coin-or.org/Dip/wiki/Dippy'
 AUTHOR_EMAIL = u''
 DESC = 'DIP Python Interface'
