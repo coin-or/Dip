17f82dac0de1ad078174b5b296cf0d4a3a0216dc
Ted Ralphs <ted@lehigh.edu>
2013-12-18 03:12:56 +0000

    temporarily committing release candidate

--- Dip/src/config_dip_default.h
+++ Dip/src/config_dip_default.h
@@ -19,14 +19,14 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define DIP_VERSION "trunk"
+#define DIP_VERSION "0.9.8"
 
 /* Major Version number of project */
-#define DIP_VERSION_MAJOR   9999
+#define DIP_VERSION_MAJOR 0
 
 /* Minor Version number of project */
-#define DIP_VERSION_MINOR   9999
+#define DIP_VERSION_MINOR 9
 
 /* Release Version number of project */
-#define DIP_VERSION_RELEASE 9999
+#define DIP_VERSION_RELEASE 8
 
