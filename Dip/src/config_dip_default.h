5fbcf94f33034eec336edfb0535708f0bb737219
Ted Ralphs <ted@lehigh.edu>
2015-10-01 14:22:43 +0000

    temporarily committing release candidate

--- Dip/src/config_dip_default.h
+++ Dip/src/config_dip_default.h
@@ -19,16 +19,16 @@
 /***************************************************************************/
 
 /* Version number of project */
-#define DIP_VERSION "trunk"
+#define DIP_VERSION "0.92.0"
 
 /* Major Version number of project */
-#define DIP_VERSION_MAJOR   9999
+#define DIP_VERSION_MAJOR 0
 
 /* Minor Version number of project */
-#define DIP_VERSION_MINOR   9999
+#define DIP_VERSION_MINOR 92
 
 /* Release Version number of project */
-#define DIP_VERSION_RELEASE 9999
+#define DIP_VERSION_RELEASE 0
 
 /* Define to 1 if the Clp package is available */
 #define DIP_HAS_CLP 1
