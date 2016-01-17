a6b9d1c63430861d3b5d3fcf339cd615edb5c2a3
Ted Ralphs <ted@lehigh.edu>
2016-01-17 19:52:02 +0000

    Ignore server certificate error

--- get.dependencies.svn.sh
+++ get.dependencies.svn.sh
@@ -5,10 +5,10 @@ do
     if [ `echo $i | cut -d '/' -f 5` == 'BuildTools' ]; then
 	if [ `echo $i | cut -d '/' -f 6` != 'stable' ]; then
 	    mkdir -p ThirdParty
-	    svn co $i ThirdParty/`echo $i | cut -d '/' -f 7`
+	    svn co --non-interactive --trust-server-cert $i ThirdParty/`echo $i | cut -d '/' -f 7`
 	fi
     else
 	proj=`echo $i | cut -d '/' -f 5`
-	svn co $i $proj
+	svn co --non-interactive --trust-server-cert $i $proj
     fi
 done
