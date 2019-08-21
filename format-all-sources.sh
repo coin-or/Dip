9cf4fe95894eb2a9cdccdc3d16fe51fdf7792e76
Simon Spoorendonk <simon@flowty.ai>
2019-08-21 22:42:30 +0200

    python formatting

--- format-all-sources.sh
+++ format-all-sources.sh
@@ -4,8 +4,10 @@
 # to .clang-format - 2019 - Haroldo
 # modified from Cbc - Simon
 
-echo formatting all source using .clang-format
+echo formatting all c++ source using .clang-format
 find ./ -iname '*.h' -o -iname '*.cpp' -exec clang-format -style=llvm -i {} +
+echo formatting all python source using black with .flake8
+black Dip/src
 
 # Check for formatting. Use this in CI pipeline
 # find ./ -iname '*.h' -o -iname '*.cpp' -exec clang-format -style=llvm -output-replacements-xml {} + | grep -c "<replacement " | grep 0 >/dev/null
